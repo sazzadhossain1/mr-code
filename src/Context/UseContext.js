7e74a93bbcdcf3ab4daa3c2f9d2d3d1aa8cbcc74
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-16 01:33:31 +0600

    create use context components and get display name

---
+++ src/Context/UseContext.js
@@ -0,0 +1,20 @@
+import React, { createContext } from "react";
+import app from "../firebase/firebase.init";
+import { getAuth } from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const UseContext = ({ children }) => {
+  const displayName = {
+    name: "Sazzad Hossain",
+  };
+
+  const authInfo = {
+    displayName,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default UseContext;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Home.css";
+import { AuthContext } from "../../Context/UseContext";
 
 const Home = () => {
+  const { displayName } = useContext(AuthContext);
+  console.log(displayName);
   return (
     <div>
       <h1>This is Home</h1>
+      <p>{displayName.name}</p>
     </div>
   );
 };
