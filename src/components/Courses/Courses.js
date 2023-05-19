1546f1cc1eeb9b335282a6d16f244163bd269bd9
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-20 02:03:01 +0600

    side navlink set by id

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -5,23 +5,23 @@ import CourseSingle from "../CourseSingle/CourseSingle";
 
 const Courses = () => {
   const getApi = useLoaderData();
-  // console.log(getApi);
+  console.log(getApi);
   return (
     <div>
       <h1 className="heading-courses">All Courses</h1>
       <div className="grid-courses-div">
         <div className="all-link-div">
           <h1>All Links</h1>
-          <Link>HTML</Link>
+          <Link to={`/courseDetailsPage/${getApi[0].id}`}>HTML</Link>
           <br />
-          <Link>Css</Link> <br />
-          <Link>Bootstrap</Link>
+          <Link to={`/courseDetailsPage/${getApi[1].id}`}>Css</Link> <br />
+          <Link to={`/courseDetailsPage/${getApi[2].id}`}>Bootstrap</Link>
           <br />
-          <Link>Tailwind</Link>
+          <Link to={`/courseDetailsPage/${getApi[3].id}`}>Tailwind</Link>
           <br />
-          <Link>Javascript</Link>
+          <Link to={`/courseDetailsPage/${getApi[4].id}`}>Javascript</Link>
           <br />
-          <Link>React</Link>
+          <Link to={`/courseDetailsPage/${getApi[5].id}`}>React</Link>
         </div>
 
         <div className="map-div container">
