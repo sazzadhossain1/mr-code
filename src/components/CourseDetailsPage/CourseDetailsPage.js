e00715393059dee6c4d87c72591142d6ed15a03f
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-20 14:39:06 +0600

    add some contant in the course details

--- src/components/CourseDetailsPage/CourseDetailsPage.js
+++ src/components/CourseDetailsPage/CourseDetailsPage.js
@@ -6,10 +6,33 @@ const CourseDetailsPage = () => {
   const courseDetailsData = useLoaderData();
   console.log(courseDetailsData);
   return (
-    <div>
-      <img src={courseDetailsData.img} alt="" />
-      <p>Name: {courseDetailsData.name}</p>
-      <p>{courseDetailsData.description}</p>
+    <div className="details-main-div">
+      <div className="details-div container">
+        <img src={courseDetailsData.img} alt="" />
+        <div className="details-text-div">
+          <p className="name">{courseDetailsData.name}</p>
+          <p className="description">Description</p>
+          <p className="lorem-description">{courseDetailsData.description}</p>
+          <div className="support-class-projects">
+            <div className="class">
+              <p>120+</p>
+              <p>Total Class</p>
+            </div>
+            <div className="projects">
+              <p>50+</p>
+              <p>Total Projects</p>
+            </div>
+            <div className="support">
+              <p>Support 9:00 - 5:00</p>
+              <p>Morning to Evening</p>
+            </div>
+            <div className="teacher">
+              <p>20+</p>
+              <p>Total Teacher</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
