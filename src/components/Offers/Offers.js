fe9025ff45d0ae0f94e15669d8c096726b625402
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-05-21 04:40:09 +0600

    add offers cart with style

--- src/components/Offers/Offers.js
+++ src/components/Offers/Offers.js
@@ -4,62 +4,65 @@ import { Link } from "react-router-dom";
 
 const Offers = () => {
   return (
-    <div className="offers-parent-div">
-      <div className="welcome-parent-div">
-        <div className="welcome-banner">
-          <h2 className="welcome-heading">
-            Welcome To Our Site <br />
-            Please Scroll
-          </h2>
+    <div>
+      <div className="offers-parent-div">
+        <div className="welcome-parent-div">
+          <div className="welcome-banner">
+            <h2 className="welcome-heading">
+              Welcome To Our Site <br />
+              Please Scroll
+            </h2>
+          </div>
         </div>
+        <h2 className="offers-heading">
+          What we Offer for Growth <br />
+          Yor Study
+        </h2>
+        <p className="offers-p">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
+          nostrum voluptas explicabo cum dolore reiciendis corrupti impedit vero
+          ipsam iusto esse sed repellat vel, excepturi et quod? Est qui vel
+          placeat soluta facere autem eligendi et quibusdam perferendis repellat
+          pariatur, sed eaque molestiae alias nulla numquam odit assumenda
+          culpa. Veniam.
+        </p>
       </div>
-      <h2 className="offers-heading">
-        What we Offer for Growth <br />
-        Yor Study
-      </h2>
-      <p className="offers-p">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-        nostrum voluptas explicabo cum dolore reiciendis corrupti impedit vero
-        ipsam iusto esse sed repellat vel, excepturi et quod? Est qui vel
-        placeat soluta facere autem eligendi et quibusdam perferendis repellat
-        pariatur, sed eaque molestiae alias nulla numquam odit assumenda culpa.
-        Veniam.
-      </p>
-
-      <div className="offers-cart-grid">
-        <div className="front-end">
-          <h2>Front-End Development</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Exercitationem sunt fugit omnis eligendi excepturi incidunt.
-          </p>
-          <Link>
-            <button>Read more</button>
-          </Link>
-          <div className="blank"></div>
-        </div>
-        <div className="back-end">
-          <h2>Back-End Development</h2>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-            laborum, repudiandae ipsa eaque est necessitatibus!
-          </p>
-          <Link>
-            <button>Read more</button>
-          </Link>
-          <div className="blank"></div>
-        </div>
-        <div className="mern-stake">
-          <h2>Mern-Stake Development</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            beatae vitae, exercitationem molestiae rem autem!
-          </p>
-          <Link>
-            <button>Read more</button>
-          </Link>
-          <div className="blank"></div>
+      <div className="auto">
+        <div className="offers-cart-grid container">
+          <div className="front-end">
+            <h2>Front-End Development</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem sunt fugit omnis eligendi excepturi incidunt.
+            </p>
+            <Link>
+              <button>Read more</button>
+            </Link>
+            <div className="blank"></div>
+          </div>
+          <div className="back-end">
+            <h2>Back-End Development</h2>
+            <p>
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+              laborum, repudiandae ipsa eaque est necessitatibus!
+            </p>
+            <Link>
+              <button>Read more</button>
+            </Link>
+            <div className="blank"></div>
+          </div>
+          <div className="mern-stake">
+            <h2>Mern-Stake Development</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              beatae vitae, exercitationem molestiae rem autem!
+            </p>
+            <Link>
+              <button>Read more</button>
+            </Link>
+            <div className="blank"></div>
+          </div>
         </div>
       </div>
     </div>
